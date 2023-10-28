cc5539b19446aad1f7cc88826d8d6b7d3a5b6ca4
Mario Manhique <79359430+MErnesto99@users.noreply.github.com>
2023-10-28 15:54:10 +0200

    added a button and a state

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mariomanhique.cicdcourse"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mariomanhique.cicdcourse"
--- app/src/main/java/com/mariomanhique/cicdcourse/MainActivity.kt
+++ app/src/main/java/com/mariomanhique/cicdcourse/MainActivity.kt
@@ -3,13 +3,27 @@ package com.mariomanhique.cicdcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mariomanhique.cicdcourse.ui.theme.CiCdCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +45,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        var textState by remember {
+            mutableStateOf("")
+        }
+        Text(
+            text = "Hello $name $textState!",
+            modifier = modifier
+        )
+        Button(onClick = {
+            textState = "Welcome to CI/CD World"
+        }, shape = RoundedCornerShape(10.dp)
+            ,modifier = Modifier.fillMaxWidth().padding(20.dp)) {
+            Text(text = "Say Hello")
+        }
+    }
 }
 
 @Preview(showBackground = true)
